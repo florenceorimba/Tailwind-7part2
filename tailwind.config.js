2ab517aaa7a52346d49b039ef51b9bffcb0395af
Flozyflo <122874219+Flozyflo@users.noreply.github.com>
2025-04-10 05:45:57 +0300

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build7a/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(33.85deg, #FDE8E4 37.88%, #ECECEC 73.61%)',
+      },
+      colors: {
+        'black-20': 'rgba(0, 0, 0, 0.2)',
+        'black-40': 'rgba(0, 0, 0, 0.4)',
+        'black-5': 'rgba(0, 0, 0, 0.05)',
+        'icon-white-100': '#FFFFFF',
+        'black-10': 'rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+  plugins: [],
+}
+
